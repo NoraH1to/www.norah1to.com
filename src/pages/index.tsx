7de2044fd9106fb073455dfb0a4de7e1a6e4aff0
NoraH1to <834053207@qq.com>
2022-01-27 16:39:28 +0800

    fix(index): 修正主页按钮跳转的路径

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className='button button--secondary button--lg'
-            to='/docs/intro'
+            to='/blog'
           >
             一些胡言乱语 🥸
           </Link>
